a1300be7df862969a22cc9da7d9cf7a7aca20b52
Tritono <info@info.it>
2018-07-03 15:48:52 +0200

    - merge branch rev03;

--- app/User.php
+++ app/User.php
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App;
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
-
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-}
-=======
 <?php
 
 namespace App;
@@ -71,4 +39,4 @@ class User extends Authenticatable
         return $this->hasOne('App\VerifyUser');
     }
 }
->>>>>>> origin/with-activaction
+
